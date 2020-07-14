16ea64ad58052f733dc7a441a317f87e4dbdcb9a
allemanfredi <alle.manfredi@gmail.com>
2020-07-14 13:13:33 +0200

    chore(global): adds config files

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# aragon-voting-reward
+# :moneybag: voting-reward
+
+An Aragon app that allows to get rewards in base of how many votes you made in a certain amount of time.
+
+&nbsp;
+
+***
+
+&nbsp;
+
+## :clipboard: How to run locally
+
+```
+yarn install
+```
+
+```
+yarn start
+```
+
+&nbsp;
+
+***
+
+&nbsp;
+
+## :guardsman: Test
+
+```
+yarn test
+```
+
+### Result
+
+```
+
+```
+
+&nbsp;
+
+***
+
+&nbsp;
+
+## :rocket: How to publish
+
+Create an __`.env`__ file with the following format
+
+```
+PRIVATE_KEY=
+INFURA_KEY=
+```
+
+Run the local IPFS node:
+
+```
+aragon ipfs start
+```
+
+and then publish.
+
+```
+npx buidler publish "version or patch/minor/major" --network "rinkeby or mainnet"
+```
---
+++ buidler.config.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const { usePlugin } = require('@nomiclabs/buidler/config')
+const hooks = require('./scripts/buidler-hooks')
+
+usePlugin('@aragon/buidler-aragon')
+
+const getEnvironmentVariable = _envVar =>
+  process.env[_envVar]
+    ? process.env[_envVar]
+    : (
+      console.error(
+        '✘ Cannot migrate!',
+        '✘ Please provide an infura api key as and an',
+        '✘ account private key as environment variables:',
+        '✘ PRIVATE_KEY',
+        '✘ INFURA_KEY'
+      ),
+      process.exit(1)
+    )
+
+module.exports = {
+  // Default Buidler configurations. Read more about it at https://buidler.dev/config/
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: 'http://localhost:8545',
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${getEnvironmentVariable('INFURA_KEY')}`,
+      accounts: [getEnvironmentVariable('PRIVATE_KEY')]
+    }
+  },
+  solc: {
+    version: '0.4.24',
+    optimizer: {
+      enabled: true,
+      runs: 10000,
+    },
+  },
+  // Etherscan plugin configuration. Learn more at https://github.com/nomiclabs/buidler/tree/master/packages/buidler-etherscan
+  etherscan: {
+    apiKey: '', // API Key for smart contract verification. Get yours at https://etherscan.io/apis
+  },
+  // Aragon plugin configuration
+  aragon: {
+    appServePort: 8001,
+    clientServePort: 3000,
+    appSrcPath: 'app/',
+    appBuildOutputPath: 'dist/',
+    appName: 'voting-reward',
+    hooks, // Path to script hooks
+  },
+}
