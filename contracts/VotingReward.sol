7a1435e2120b40e661c79772cdcb8b9e887b7a4e
allemanfredi <alle.manfredi@gmail.com>
2020-07-14 22:16:37 +0200

    feat(contracts): adds claimRewards skeleton

--- contracts/VotingReward.sol
+++ contracts/VotingReward.sol
@@ -29,44 +29,57 @@ contract VotingReward is AragonApp {
     // prettier-ignore
     string private constant ERROR_VAULT_INSUFFICENT_TOKENS = "VOTING_REWARD_VAULT_INSUFFICENT_TOKENS";
     // prettier-ignore
-    string private constant ERROR_VAULT_NO_VOTES = "VOTING_REWARD_VAULT_NO_VOTES";
+    string private constant ERROR_VOTING_NO_VOTES = "VOTING_REWARD_VOTING_NO_VOTES";
+    // prettier-ignore
+    string private constant ERROR_SENDER_NOT_VOTED = "VOTING_REWARD_NOT_VOTED";
     // prettier-ignore
     string private constant ERROR_TOO_MUCH_MISSING_VOTES = "VOTING_REWARD_TOO_MUCH_MISSING_VOTES";
+    // prettier-ignore
+    string private constant ERROR_EPOCH_NOT_REACHED = "VOTING_REWARD_EPOCH_NOT_REACHED";
 
-    Vault public vault;
+    Vault public baseVault;
+    Vault public rewardsVault;
     Voting public voting;
 
-    address public rewardToken;
-    uint64 public minSecondsThreeshold;
+    address public rewardsToken;
+    uint64 public epoch;
+    uint64 private deployDate;
 
-    mapping(address => uint64) public addressDateLastVote;
+    mapping(address => uint64) public lastDateClaimedRewards;
+    mapping(address => uint64) public addressDateLastDistributedReward;
 
-    event MinSecondsThreesholdChanged(uint64 minSecondsThreeshold);
-    event VaultChanged(address vault);
+    event EpochChanged(uint64 epoch);
+    event BaseVaultChanged(address baseVault);
+    event RewardVaultChanged(address rewardsVault);
     event VotingChanged(address voting);
     event RewardDistributed(address beneficiary, uint256 amount);
 
     /**
      * @notice Initialize VotingReward app contract
-     * @param _vault Vault address
+     * @param _baseVault Vault address from which token are taken
+     * @param _rewardsVault Vault address to which token are put
      * @param _voting Voting address
-     * @param _rewardToken Accepted token address
-     * @param _minSecondsThreeshold number of seconds minimun to have access to voting rewards
+     * @param _rewardsToken Accepted token address
+     * @param _epoch number of seconds minimun to have access to voting rewards
      */
     function initialize(
-        address _vault,
+        address _baseVault,
+        address _rewardsVault,
         address _voting,
-        address _rewardToken,
-        uint64 _minSecondsThreeshold
+        address _rewardsToken,
+        uint64 _epoch
     ) external onlyInit {
-        require(isContract(_vault), ERROR_ADDRESS_NOT_CONTRACT);
+        require(isContract(_baseVault), ERROR_ADDRESS_NOT_CONTRACT);
+        require(isContract(_rewardsVault), ERROR_ADDRESS_NOT_CONTRACT);
         require(isContract(_voting), ERROR_ADDRESS_NOT_CONTRACT);
-        require(isContract(_rewardToken), ERROR_ADDRESS_NOT_CONTRACT);
+        require(isContract(_rewardsToken), ERROR_ADDRESS_NOT_CONTRACT);
 
-        vault = Vault(_vault);
+        baseVault = Vault(_baseVault);
+        rewardsVault = Vault(_rewardsVault);
         voting = Voting(_voting);
-        rewardToken = _rewardToken;
-        minSecondsThreeshold = _minSecondsThreeshold;
+        rewardsToken = _rewardsToken;
+        epoch = _epoch;
+        deployDate = getTimestamp64();
 
         initialized();
     }
@@ -74,91 +87,120 @@ contract VotingReward is AragonApp {
     /**
      * @notice Check if msg.sender is able to be rewarded, and in positive case,
      *         he will be funded with the corresponding earned amount of tokens
+     * @param _beneficiary address to which the deposit will be transferred if successful
      */
-    function claimRewards() external view {
+    function claimReward(address _beneficiary) external {
         uint256 votesLength = voting.votesLength();
-        require(votesLength > 0, ERROR_VAULT_NO_VOTES);
+        require(votesLength > 0, ERROR_VOTING_NO_VOTES);
 
-        // threeshold is (getTimestamp64() - dateLastProposal) / minSecondsThreeshold
-        // where dateLastProposal is equal to getTimestamp64() if a msg.sender
-        // never claimed a reward in order to be able to catch all previous proposals
-        uint64 dateLastProposal = getTimestamp64();
-        if (addressDateLastVote[msg.sender] != 0) {
-            dateLastProposal = addressDateLastVote[msg.sender];
+        uint64 timestamp = getTimestamp64();
+
+        uint64 lastDateClaimedReward = 0;
+        if (lastDateClaimedRewards[_beneficiary] != 0) {
+            lastDateClaimedReward = lastDateClaimedRewards[_beneficiary];
+        } else {
+            lastDateClaimedReward = deployDate;
         }
 
-        uint64 timeThreeshold = getTimestamp64().sub(dateLastProposal);
-        uint64 missVotesThreeshold = timeThreeshold.div(minSecondsThreeshold);
+        // must be passed at least one epoch before requesting another reward
+        require(
+            timestamp - lastDateClaimedReward >= epoch,
+            ERROR_EPOCH_NOT_REACHED
+        );
+
+        // missingVotesThreeshold is (now - lastDateClaimedReward) / epoch
+        // where lastDateClaimedReward is equal to deployDate if a _beneficiary
+        // never claimed a reward in order to be able to catch all previous proposals
+        uint64 missingVotesThreeshold = timestamp
+            .sub(lastDateClaimedReward)
+            .div(epoch);
+
         uint64 recentVote = 0;
-        uint256 missVotes = 0;
+        uint256 missingVotes = 0;
         // TODO: calculate reward correctly
         uint256 reward = 10;
 
-        for (uint256 voteId; voteId < votesLength; voteId++) {
+        for (uint256 voteId = 0; voteId < votesLength; voteId++) {
             uint64 startDate;
             (, , startDate, , , , , , , ) = voting.getVote(voteId);
 
             // if a vote is within the threeshold
-            if (startDate >= timeThreeshold) {
+            if (startDate >= lastDateClaimedReward) {
                 Voting.VoterState state = voting.getVoterState(
                     voteId,
-                    msg.sender
+                    _beneficiary
                 );
 
-                // if msg.sender has voted
+                // if _beneficiary has voted
                 if (state != Voting.VoterState.Absent) {
                     if (startDate > recentVote) {
                         recentVote = startDate;
                     }
                 } else {
-                    missVotes = missVotes.add(1);
+                    missingVotes = missingVotes.add(1);
                 }
 
-                // avoid cycles when there is more than missVotesThreeshold missing votes
+                // avoid cycles when there is more than missingVotesThreeshold missing votes
                 require(
-                    missVotes <= missVotesThreeshold,
+                    missingVotes <= missingVotesThreeshold,
                     ERROR_TOO_MUCH_MISSING_VOTES
                 );
             }
         }
 
-        // be sure that vault contains at least this reward
+        // be sure that baseVault contains at least this reward
         require(
-            ERC20(rewardToken).balanceOf(vault) >= reward,
+            ERC20(rewardsToken).balanceOf(baseVault) >= reward,
             ERROR_VAULT_INSUFFICENT_TOKENS
         );
 
-        addressDateLastVote[msg.sender] = recentVote;
+        lastDateClaimedRewards[_beneficiary] = recentVote;
 
-        emit RewardDistributed(msg.sender, reward);
+        // TODO: send reward to vaultReward
+
+        emit RewardDistributed(_beneficiary, reward);
     }
 
     /**
      * @notice Change minimum number of seconds to have access to voting rewards
-     * @param _minSecondsThreeshold number of seconds minimun to have access to voting rewards
+     * @param _epoch number of seconds minimun to have access to voting rewards
      */
-    function changeMinSecondsThreeshold(uint64 _minSecondsThreeshold)
+    function changeEpoch(uint64 _epoch)
         external
         auth(CHANGE_MIN_SECONDS_THREESOLD)
     {
-        require(
-            _minSecondsThreeshold > 0,
-            ERROR_MIN_SECONDS_THREESHOLD_NOT_ZERO
-        );
-        minSecondsThreeshold = _minSecondsThreeshold;
+        require(_epoch > 0, ERROR_MIN_SECONDS_THREESHOLD_NOT_ZERO);
+        epoch = _epoch;
 
-        emit MinSecondsThreesholdChanged(_minSecondsThreeshold);
+        emit EpochChanged(_epoch);
     }
 
     /**
-     * @notice Change Vault
-     * @param _vault new vault address
+     * @notice Change Base Vault
+     * @param _baseVault new base vault address
      */
-    function changeVault(address _vault) external auth(CHANGE_VAULT_ROLE) {
-        require(isContract(_vault), ERROR_ADDRESS_NOT_CONTRACT);
-        vault = Vault(_vault);
+    function changeBaseVault(address _baseVault)
+        external
+        auth(CHANGE_VAULT_ROLE)
+    {
+        require(isContract(_baseVault), ERROR_ADDRESS_NOT_CONTRACT);
+        baseVault = Vault(_baseVault);
 
-        emit VaultChanged(_vault);
+        emit BaseVaultChanged(_baseVault);
+    }
+
+    /**
+     * @notice Change Reward Vault
+     * @param _rewardsVault new reward vault address
+     */
+    function changeRewardVault(address _rewardsVault)
+        external
+        auth(CHANGE_VAULT_ROLE)
+    {
+        require(isContract(_rewardsVault), ERROR_ADDRESS_NOT_CONTRACT);
+        rewardsVault = Vault(_rewardsVault);
+
+        emit RewardVaultChanged(_rewardsVault);
     }
 
     /**
